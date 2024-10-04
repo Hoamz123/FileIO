6224510a9f263835312d3e5ff90ee334c26740b1
Hoamz123 <dvhoa2171@gmail.com>
2024-10-04 23:39:11 +0700

    hoa da thay doi vao 4/10

--- src/File/ReadFile.java
+++ src/File/ReadFile.java
@@ -1,23 +1,24 @@
 package File;
 
 import java.io.File;
-import java.io.IOException;
+
 import java.util.Scanner;
 
 public class ReadFile {
     public static void main(String[] args) {
         try{
-            Scanner sc = new Scanner(new File("input.txt"));
-            String line = sc.nextLine();
-            System.out.println(line);
-            int n = Integer.parseInt(sc.nextLine());
-            System.out.println(n);
-            String s = sc.nextLine();
-            System.out.println(s);
+            Scanner sc = new Scanner(new File("output.txt"));
+            while(sc.hasNext()){
+                try{
+                    int x = Integer.parseInt(sc.next());
+                    System.out.print(x + " ");
+                }catch(Exception e){
+                    continue;
+                }
+            }
             sc.close();
-        }catch (IOException e){
-            System.out.println(e.getMessage());//pull da update
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
-
     }
 }
